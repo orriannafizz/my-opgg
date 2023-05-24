61861e4d0d93c76609aaff6b3576720e4f406ff3
orriannafizz <orriannafizz@gmail.com>
2023-05-24 18:09:26 +0800

    build: championMap, champion.json

---
+++ hooks/championMap.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+import championData from '../data/champions.json';
+
+interface Champion {
+  version: string;
+  id: string;
+  key: string;
+  name: string;
+  title: string;
+  blurb: string;
+  info: Object;
+  image: Object;
+  tags: Array<string>;
+  partype: string;
+  stats: Object;
+}
+
+const useChampionMap = () => {
+  const [championMap, setChampionMap] = useState<Map<string, string>>();
+
+  useEffect(() => {
+    const tempMap = new Map<string, string>();
+    const champions = championData.data as { [key: string]: Champion };
+    for (const champ of Object.values(champions)) {
+      tempMap.set(champ.key, champ.name);
+    }
+    setChampionMap(tempMap);
+  }, []);
+
+  return championMap;
+};
+
+export default useChampionMap;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { Inter } from 'next/font/google';
 import SummonerInfo from '../../components/SummonerInfo';
-
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
